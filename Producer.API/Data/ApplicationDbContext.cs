8f262a6ab85e7b19f61f95fb76cfc9692d2bd60d
Simuzeche Kaluwa <simuzechek@gmail.com>
2022-07-28 16:01:09 +0200

    Added DB Context, removed bioler plate code, added EF COre to DI container

---
+++ Producer.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Producer.API.Models;
+
+namespace Producer.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+        
+        }
+
+        public DbSet<Product> Properties { get; set; }
+    }
+}
+    
