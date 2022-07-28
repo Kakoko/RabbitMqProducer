768cecdef4207ffbf4f3d3dc0b0cf0932ecfa09d
Simuzeche Kaluwa <simuzechek@gmail.com>
2022-07-28 15:35:37 +0200

    Feat: Added Product Class

---
+++ Producer.API/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Producer.API.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = String.Empty;
+        public string ProductDescription { get; set; } = String.Empty;
+        public int ProductPrice { get; set; }
+        public int ProductStock { get; set; }
+    }   
+}
