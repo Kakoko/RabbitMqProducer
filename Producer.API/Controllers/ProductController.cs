0a9b035891a70f7f1b508b4cf9d5d6cdb1b0a83e
Simuzeche Kaluwa <simuzechek@gmail.com>
2022-07-28 16:39:58 +0200

    Feat: Implemented Product Controller

--- Producer.API/Controllers/ProductController.cs
+++ Producer.API/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Producer.API.Models;
+using Producer.API.RabbitMq;
+using Producer.API.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +11,47 @@ namespace Producer.API.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        // GET: api/<ProductController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly IProductService _productService;
+        private readonly IRabitMQProducer _rabitMQProducer;
+
+        public ProductController(IProductService productService , IRabitMQProducer rabitMQProducer)
         {
-            return new string[] { "value1", "value2" };
+            _productService = productService;
+            _rabitMQProducer = rabitMQProducer;
         }
 
-        // GET api/<ProductController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("productlist")]
+        public IEnumerable<Product> ProductList()
         {
-            return "value";
+            var productList = _productService.GetProductList();
+            return productList;
         }
 
-        // POST api/<ProductController>
-        [HttpPost]
-        public void Post([FromBody] string value)
+        [HttpGet("getproductbyid")]
+        public Product GetProductById(int Id)
         {
+            return _productService.GetProductById(Id);
         }
 
-        // PUT api/<ProductController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPost("addproduct")]
+        public Product AddProduct(Product product)
         {
+            var productData = _productService.AddProduct(product);
+            //send the inserted product data to the queue and consumer will listening this data from queue
+            _rabitMQProducer.SendProductMessage(productData);
+            return productData;
         }
 
-        // DELETE api/<ProductController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpPut("updateproduct")]
+        public Product UpdateProduct(Product product)
         {
+            return _productService.UpdateProduct(product);
+        }
+
+        [HttpDelete("deleteproduct")]
+        public bool DeleteProduct(int Id)
+        {
+            return _productService.DeleteProduct(Id);
         }
     }
 }
